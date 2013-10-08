84045f9dbe64a5326aee5c776b92d8a7aeca36bb
Miguel Roman <miguelerm@gmail.com>
2013-10-08 13:54:41 -0600

    utilizando ng-repeat

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 function mainController($scope) {
+    
+    $scope.numeros = [];
     $scope.numero = 100;
-    $scope.numeroAlCuadrado = function () {
-        return $scope.numero * $scope.numero;
+    
+    $scope.numeroAlCuadrado = function (numero) {
+        return numero * numero;
     };
+    
+    $scope.agregarNumero = function () {
+        var numero = $scope.numero;
+        if (numero && $scope.numeros.indexOf(numero) === -1) {
+            $scope.numeros.push(numero);
+            $scope.numero = null;
+        }
+    };
+    
+    $scope.eliminarNumero = function (numero) {
+        var index = $scope.numeros.indexOf(numero);
+        if (index >= 0) {
+            $scope.numeros.splice(index, 1);
+        }
+    };
+    
 }
